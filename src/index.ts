2fe24f61eafb7844de7f8ee15cf903105042df92
Samiksha <samikshatrivedi007@gmail.com>
2025-08-07 21:18:34 +0530

    intial commit

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 app.get('/', (_req, res) => {
-    res.send('Roxiler backend running 🚀');
+    res.send('Roxiler backend running !!!');
 });
 
 app.listen(PORT, () => {
