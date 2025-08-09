b7ffdd58dc0ba745b1df06f29b9b3a4a9b7caf3e
Samiksha <samikshatrivedi007@gmail.com>
2025-08-09 19:17:11 +0530

    auth routes code updated

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -2,8 +2,7 @@ import { Router } from "express";
 import { register, login } from "../controllers/auth.controller";
 
 const router = Router();
-
-router.post("/register", register); // register normal user or admin (admin route only for admin in real app)
+router.post("/register", register);
 router.post("/login", login);
 
 export default router;
