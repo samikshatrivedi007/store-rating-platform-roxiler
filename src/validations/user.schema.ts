6f37028b101a5baae5957aebd2cd1d5105247bcb
Samiksha <samikshatrivedi007@gmail.com>
2025-08-10 14:35:27 +0530

    code updated

---
+++ src/validations/user.schema.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+export const createUserSchema = z.object({
+    name: z.string()
+        .min(20, "Name must contain at least 20 characters")
+        .max(60,"Name must be not no more than 60 characters"),
+    email: z.string().email("Invalid email address"),
+    address: z.string()
+        .max(400,"address must be not more than 400 characters"),
+    role: z.enum(["ADMIN", "STORE_OWNER", "CUSTOMER"]),
+    password: z.string()
+        .min(8, "Password must be at least 8 characters")
+        .max(16,"Password must be not more than 12 characters"),
+
+});
+
+export const updateUserSchema = createUserSchema.partial();
