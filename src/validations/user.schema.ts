3d39bc54335c4c341e0f64b7be9f4f72954d797c
Samiksha <samikshatrivedi007@gmail.com>
2025-08-10 15:42:13 +0530

    code updated

--- src/validations/user.schema.ts
+++ src/validations/user.schema.ts
@@ -1,19 +1,20 @@
 import { z } from "zod";
 
-export const
-    loginBodySchema = z.object({
+export const registerSchema = z.object({
     name: z.string()
         .min(20, "Name must contain at least 20 characters")
-        .max(60,"Name must be not no more than 60 characters"),
-    email: z.email("Invalid email address"),
-    address: z.string()
-        .max(400,"address must be not more than 400 characters"),
+        .max(60, "Name must be not more than 60 characters"),
+    email: z.string().email("Invalid email address"),
+    address: z.string().max(400, "Address must be not more than 400 characters").optional(),
     role: z.enum(["ADMIN", "STORE_OWNER", "CUSTOMER"]),
     password: z.string()
         .min(8, "Password must be at least 8 characters")
-        .max(16,"Password must be not more than 16 characters"),
-
+        .max(16, "Password must be not more than 16 characters"),
 });
 
-export const updateUserSchema =
-    loginBodySchema.partial();
+export const loginSchema = z.object({
+    email: z.string().email("Invalid email address"),
+    password: z.string()
+        .min(8, "Password must be at least 8 characters")
+        .max(16, "Password must be not more than 16 characters"),
+});
