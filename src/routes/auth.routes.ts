983e1ed76b68932bbeea351b85f42317d28764db
Samiksha <samikshatrivedi007@gmail.com>
2025-08-10 15:34:40 +0530

    code updated

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { register, login } from "../controllers/auth.controller";
-import {loginBodySchema} from "../validations/user.schema";
+
 
 const router = Router();
 router.post("/register", register);
